bbe5173290d701d325b9f4fda4a39549a6c7942a
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-08-05 18:57:18 -0300

    feat: Adds a User Model

---
+++ src/main/java/com/hardware_today/model/UserModel.java
@@ -0,0 +1,15 @@
+package com.hardware_today.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserModel {
+
+}
