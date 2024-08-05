7f839458c847da8804d8a3d95eaf733c9cf914d4
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-08-05 18:51:41 -0300

    feat: Creates a User Controller

---
+++ src/main/java/com/hardware_today/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.hardware_today.controller;
+
+import com.hardware_today.model.UserModel;
+import com.hardware_today.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
