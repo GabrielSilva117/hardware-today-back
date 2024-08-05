636b82837bafa3e92b50cdc3b63175f4569753f4
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-08-05 18:53:14 -0300

    feat: Creates the User Service

---
+++ src/main/java/com/hardware_today/service/UserService.java
@@ -0,0 +1,14 @@
+package com.hardware_today.service;
+
+import com.hardware_today.entity.UserEntity;
+import com.hardware_today.model.UserModel;
+import com.hardware_today.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
+
