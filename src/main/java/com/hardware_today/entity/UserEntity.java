add5b01e9d2138908ebe0c9ff6b07dba307bcded
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-08-05 18:52:47 -0300

    feat: Creates a User entity

---
+++ src/main/java/com/hardware_today/entity/UserEntity.java
@@ -0,0 +1,19 @@
+package com.hardware_today.entity;
+
+import com.hardware_today.model.UserModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    
+}
---
+++ src/main/java/com/hardware_today/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hardware_today.repository;
+
+import com.hardware_today.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
