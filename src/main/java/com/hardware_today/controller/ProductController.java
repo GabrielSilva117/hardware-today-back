72b90c32d5a5f5b5d3c9e03c9a30a44103c5202c
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-11-18 18:55:02 -0300

    feat: Adds a filter logic to the product list endpoint

--- src/main/java/com/hardware_today/controller/ProductController.java
+++ src/main/java/com/hardware_today/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.hardware_today.controller;
 
 import com.hardware_today.entity.ProductEntity;
+import com.hardware_today.model.ProductFilterModel;
 import com.hardware_today.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +19,12 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @GetMapping
-    public List<ProductEntity> getAllProducts() {
+    public List<ProductEntity> getAllProducts(@RequestBody(required = false) ProductFilterModel filter) {
+        if (filter != null) {
+            return productRepository.findAllFilteredValues(filter.getBrandUUIDs(), filter.getCategoryUUIDs(),
+                    filter.getMinPrice(), filter.getMaxPrice());
+        }
+
         return productRepository.findAll();
     }
 }
--- src/main/java/com/hardware_today/repository/ProductRepository.java
+++ src/main/java/com/hardware_today/repository/ProductRepository.java
@@ -2,8 +2,17 @@ package com.hardware_today.repository;
 
 import com.hardware_today.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    @Query(value = "SELECT p FROM product p where (:brand IS NULL OR p.brand.id IN (:brands))" +
+            " AND (:category IS NULL OR p.category.id IN (:categories)) " +
+            "AND p.price BETWEEN COALESCE(:minPrice, 0) AND COALESCE(:maxPrice, 9999)")
+    List<ProductEntity> findAllFilteredValues(@Param("brands") String brands, @Param("categories") String categories,
+                                            @Param("minPrice") String minPrice, @Param("maxPrice") String maxPrice);
 }
