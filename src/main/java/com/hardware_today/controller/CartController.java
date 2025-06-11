f1bb5f6d4243732f04e5bdc8b14ff75f26aa4c31
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-06-10 21:22:27 -0300

    feat: Adds the deleteMapping import

--- src/main/java/com/hardware_today/controller/CartController.java
+++ src/main/java/com/hardware_today/controller/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
