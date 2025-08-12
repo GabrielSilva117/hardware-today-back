2c4a398f304ff182e5433e29b0a09895f455a160
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-08-11 21:20:01 -0300

    refactor: Implements the entity name to the CartItem model class

--- src/main/java/com/hardware_today/entity/CartItem.java
+++ src/main/java/com/hardware_today/entity/CartItem.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
-@Entity
+@Entity(name = "cart_item")
 @Getter
 @Setter
 @AllArgsConstructor
