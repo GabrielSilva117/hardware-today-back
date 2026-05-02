c03cae74d83d8ef6ffb31a6c3743d35b30e13547
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2026-05-02 17:19:55 -0300

    feat: Adds the payment ms bindings

--- src/main/java/com/hardware_today/config/MQConfig.java
+++ src/main/java/com/hardware_today/config/MQConfig.java
@@ -1,26 +1,36 @@
 package com.hardware_today.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MQConfig {
+    public static String PAYMENT_EXCHANGE = "payment.exchange";
     public static String NOTIFICATION_EXCHANGE = "notification.exchange";
+
+    public static String PAYMENT_QUEUE = "payment_dispatch";
     public static String EMAIL_QUEUE = "email_dispatch";
+
     public static String EMAIL_ROUTING_KEY = "email.create";
 
     @Bean
-    public DirectExchange notificationExchange() {
-        return new DirectExchange(NOTIFICATION_EXCHANGE);
-    }
+    public DirectExchange notificationExchange() { return new DirectExchange(NOTIFICATION_EXCHANGE); }
 
     @Bean
-    public Queue emailQueue() {
-        return new Queue(EMAIL_QUEUE, true); // durable queue
+    public TopicExchange paymentExchange() { return new TopicExchange(PAYMENT_EXCHANGE); }
+
+    @Bean
+    public Queue emailQueue() { return new Queue(EMAIL_QUEUE, true); }
+
+    @Bean
+    public Queue paymentQueue() { return new Queue(PAYMENT_QUEUE, true); }
+
+    @Bean
+    public Binding paymentBinding() {
+        return BindingBuilder.bind(paymentQueue())
+                .to(paymentExchange())
+                .with("payment.#");
     }
 
     @Bean
