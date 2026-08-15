8295f60b10be38c93826c0766d5e92b02b6cdd4a
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2026-08-15 16:20:50 -0300

    feat: Enables pagination on Spring

--- src/main/java/com/hardware_today/EcommerceApplication.java
+++ src/main/java/com/hardware_today/EcommerceApplication.java
@@ -3,9 +3,11 @@ package com.hardware_today;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
