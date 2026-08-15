f9b2737bb2b2b4667245058a80eb59f0aa9574f4
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2026-08-15 16:19:41 -0300

    Feat: Adds the product search and implements the specification
    This refactor the filter to work along side the search logic

--- src/main/java/com/hardware_today/controller/ProductController.java
+++ src/main/java/com/hardware_today/controller/ProductController.java
@@ -3,12 +3,12 @@ package com.hardware_today.controller;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hardware_today.projections.ProductProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.hardware_today.entity.Product;
 import com.hardware_today.model.ProductFilterModel;
@@ -21,18 +21,26 @@ public class ProductController {
 	
 	public ProductController(ProductService productService) {
 		this.productService = productService;
-	}    
+	}
 
+//    public List<Product> getAllProducts(@RequestBody(required = false) ProductFilterModel filter) {
+//    	try {
+//    		return this.productService.getAllProducts(filter);
+//    	} catch (Exception err) {
+//    		err.printStackTrace();
+//    		return null;
+//    	}
+//    }
     @PostMapping
-    public List<Product> getAllProducts(@RequestBody(required = false) ProductFilterModel filter) {
-    	try {
-    		return this.productService.getAllProducts(filter);
-    	} catch (Exception err) {
-    		err.printStackTrace();
-    		return null;
-    	}
+    public ResponseEntity<Page<ProductProjection>> getAllProducts(
+            @RequestBody ProductFilterModel filter,
+            @RequestParam(defaultValue = "0", required = false) int page,
+            @RequestParam(defaultValue = "20", required = false) int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(productService.getAllProducts(filter, pageable));
     }
-    
+
     @GetMapping("/{productId}")
     public Product getProductById(@PathVariable UUID productId) {
     	try {
--- src/main/java/com/hardware_today/service/ProductService.java
+++ src/main/java/com/hardware_today/service/ProductService.java
@@ -3,6 +3,11 @@ package com.hardware_today.service;
 import java.util.List;
 import java.util.UUID;
 
+import com.hardware_today.projections.ProductProjection;
+import com.hardware_today.specifications.ProductSpecification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.hardware_today.entity.Product;
@@ -18,17 +23,24 @@ public class ProductService {
 		this.productRepository = productRepository;
 	}
 	
-	public List<Product> getAllProducts(ProductFilterModel filter) {
-        if (filter != null) {
-            return productRepository.getAllProducts(
-            		ServiceUtils.parseCommaSeparatedUUID(filter.getBrand()), 
-            		ServiceUtils.parseCommaSeparatedUUID(filter.getCategory()),
-                    filter.getMinPrice(), 
-                    filter.getMaxPrice()
-            );
-        }
+//	public List<Product> getAllProducts(ProductFilterModel filter) {
+//        if (filter != null) {
+//            return productRepository.getAllProducts(
+//            		ServiceUtils.parseCommaSeparatedUUID(filter.getBrand()),
+//            		ServiceUtils.parseCommaSeparatedUUID(filter.getCategory()),
+//                    filter.getMinPrice(),
+//                    filter.getMaxPrice()
+//            );
+//        }
+//
+//        return productRepository.findAll();
+//    }
 
-        return productRepository.findAll();
+    public Page<ProductProjection> getAllProducts(ProductFilterModel filter, Pageable pageable) {
+        if (filter == null) filter = new ProductFilterModel();
+
+        Specification<Product> spec = ProductSpecification.build(filter);
+        return productRepository.findBy(spec, q -> q.as(ProductProjection.class).page(pageable));
     }
 	
 	public Product getProductById(UUID id) {
